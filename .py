5d545872760d408bd7671a86d75055eb2af0d784
Kond Vinay <109887287+20r01a04l8@users.noreply.github.com>
2025-07-31 10:21:18 +0530

    Perfect_guess_Adv .py

--- .py
+++ .py
@@ -1,16 +1,54 @@
 import random
 
+# For selecting choices
+def select_choices():
+    print("Enter the choice: ")
+    print("Easy level: (1-20)")
+    print("Medium level: (21-100)")
+    print("Hard level: (100-500)")
+    while True:
+        choice = input("Enter your choice: ")
+        try:
+            if choice=="1":
+                return 1,20
+            elif choice=="2":
+                return 21,100
+            elif choice=="3":
+                return 100,500
+            else:
+                print("Invalid choice. Please enter 1, 2 or 3.")
+        except ValueError:
+            print("Invalid value or choice. Please enter 1, 2 or 3.")
 
-number = random.randint(1 , 100)
-attempt = 1
-guess = int(input("Guess the number : "))
-while(True):
-    if(guess>number):
-        guess = int(input("The Number is bigger than the number! Try again: "))
-        attempt +=1
-    elif(guess<number):
-        guess = int(input("The Number is smaller than the number! Try again: "))
-        attempt +=1
-    else:
-        print(f"YAY! You guessed it right in {attempt} attempts")
-        break
+#Function for playing game
+def play_game():
+    low,high = select_choices()
+    number_to_guess = random.randint(low,high)
+    attempt=0
+    print(f"Ok! I'm thinking of a number between {low} and {high}.")
+    print("You have to guess it. After each guess, I'll tell you if your guess was too high or too low.")
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+            attempt+=1
+            if guess < low or guess > high:
+                print("Please enter a number between",low,"and",high,".")
+            elif guess < number_to_guess:
+                print("Too low!")
+            elif guess > number_to_guess:
+                print("Too high!")
+            else:
+                print("Congratulations! You guessed the number correctly in ",attempt,"attempts.")
+                break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+# Main Function
+def main():
+    play_game()
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again.lower() == "yes":
+        main()
+
+if __name__ == "__main__":
+    main()
